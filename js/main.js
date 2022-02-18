e2faf0169ec9e7bd9fcfe3b783142727cf50604f
shadmani23 <shadmani.237@gmail.com>
2022-02-18 08:10:52 +0530

    Added case study

--- js/main.js
+++ js/main.js
@@ -1,67 +1,98 @@
-/*
-* Template Name: MyPortfolio
-* Template URL: https://bootstrapmade.com/myportfolio-bootstrap-portfolio-website-template/
-* License: https://bootstrapmade.com/license/
-*/
+(function ($) {
+  "use strict";
+
+  var burgerMenu = function () {
+    $(".burger").click(function (e) {
+      $(window).scrollTop(0);
+      if (!$(".burger").hasClass("active")) $(".burger").addClass("active");
+      else $(".burger").removeClass("active");
+    });
+  };
+  burgerMenu();
+
+  var siteIstotope = function () {
+    var $container = $("#portfolio-grid, #portfolio-grid2").isotope({
+      itemSelector: ".item",
+      isFitWidth: true,
+    });
+
+    $(window).resize(function () {
+      $container.isotope({
+        columnWidth: ".col-sm-3",
+      });
+    });
+
+    $container.isotope({ filter: "*" });
+
+    $("#filters").on("click", "a", function (e) {
+      e.preventDefault();
+      var filterValue = $(this).attr("data-filter");
+      $container.isotope({ filter: filterValue });
+      $("#filters a").removeClass("active");
+      $(this).addClass("active");
+    });
+  };
+  $(window).on("load", function () {
+    siteIstotope();
+  });
+
+  var siteOwlCarousel = function () {
+    $(".testimonial-carousel").owlCarousel({
+      center: true,
+      items: 1,
+      loop: true,
+      margin: 0,
+      autoplay: true,
+      smartSpeed: 1000,
+    });
+  };
+  siteOwlCarousel();
+})(jQuery);
+
+AOS.init({
+  easing: "ease",
+  duration: 1000,
+  once: true,
+});
+
+//Scroll back to top
 
 (function ($) {
   "use strict";
 
-  var burgerMenu = function() {
-	  $('.burger').click(function(e) {
-	  	$(window).scrollTop(0);
-	    if(!$('.burger').hasClass('active'))
-	      $('.burger').addClass('active');
-	    else
-	      $('.burger').removeClass('active');
-	  });
-  }
-  burgerMenu();
+  $(document).ready(function () {
+    "use strict";
 
-  var siteIstotope = function() {
-	  var $container = $('#portfolio-grid').isotope({
-	    itemSelector : '.item',
-	    isFitWidth: true
-	  });
-
-	  $(window).resize(function(){
-	    $container.isotope({
-	      columnWidth: '.col-sm-3'
-	    });
-	  });
-	  
-	  $container.isotope({ filter: '*' });
-
-	  $('#filters').on( 'click', 'a', function(e) {
-	  	e.preventDefault();
-	    var filterValue = $(this).attr('data-filter');
-	    $container.isotope({ filter: filterValue });
-	    $('#filters a').removeClass('active');
-	    $(this).addClass('active');
-	  });
-  }
-  $(window).on('load', function () {
-    siteIstotope();
+    var progressPath = document.querySelector(".progress-wrap path");
+    var pathLength = progressPath.getTotalLength();
+    progressPath.style.transition = progressPath.style.WebkitTransition =
+      "none";
+    progressPath.style.strokeDasharray = pathLength + " " + pathLength;
+    progressPath.style.strokeDashoffset = pathLength;
+    progressPath.getBoundingClientRect();
+    progressPath.style.transition = progressPath.style.WebkitTransition =
+      "stroke-dashoffset 10ms linear";
+    var updateProgress = function () {
+      var scroll = $(window).scrollTop();
+      var height = $(document).height() - $(window).height();
+      var progress = pathLength - (scroll * pathLength) / height;
+      progressPath.style.strokeDashoffset = progress;
+    };
+    updateProgress();
+    $(window).scroll(updateProgress);
+    var offset = 50;
+    var duration = 550;
+    jQuery(window).on("scroll", function () {
+      if (jQuery(this).scrollTop() > offset) {
+        jQuery(".progress-wrap").addClass("active-progress");
+      } else {
+        jQuery(".progress-wrap").removeClass("active-progress");
+      }
+    });
+    jQuery(".progress-wrap").on("click", function (event) {
+      event.preventDefault();
+      jQuery("html, body").animate({ scrollTop: 0 }, duration);
+      return false;
+    });
   });
-
-
-  var siteOwlCarousel = function() {
-  	$('.testimonial-carousel').owlCarousel({
-		  center: true,
-	    items: 1,
-	    loop: true,
-	    margin: 0,
-	    autoplay: true,
-	    smartSpeed: 1000,
-		});
-  };
-  siteOwlCarousel();
-
-
 })(jQuery);
-
-AOS.init({
-	easing: 'ease',
-	duration: 1000,
-	once: true
-});
